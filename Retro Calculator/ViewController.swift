32d2815479ed40283bba10bd1da803d9c1f09102
Lalit Bagga <Davin12x@Lalits-MacBook-Pro.local>
2016-01-07 02:03:15 -0500

    Action Function Added
    Current button pressed can be seen on calculator

--- Retro Calculator/ViewController.swift
+++ Retro Calculator/ViewController.swift
@@ -11,6 +11,19 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    enum Operation:String{
+        case Divide = "/"
+        case Multiply = "*"
+        case Addition = "+"
+        case Substract = "-"
+        case Empty = " "
+        case Equal = "="
+        
+    }
+    var runningNumber = ""
+    var leftVar = ""
+    var rightVar = ""
+    var currentOperation : Operation = Operation.Empty
     @IBOutlet weak var outputLabel:UILabel!
     var btnSound:AVAudioPlayer!
 
@@ -31,11 +44,43 @@ class ViewController: UIViewController {
     }
     @IBAction func numberPressed(btn:UIButton)
     {
-        btnSound.play()
+       playSound()
+        runningNumber += "\(btn.tag)"
+        outputLabel.text = runningNumber
     }
 
    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
 
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+    }
 
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(Operation.Substract)
+    }
+    
+    @IBAction func AddPressed(sender: AnyObject) {
+        processOperation(Operation.Addition)
+    }
+   
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(Operation.Equal)
+    }
+    
+    func processOperation(op:Operation)
+    {
+        playSound()
+    }
+    
+    func playSound(){
+        if btnSound.playing{
+            btnSound.stop()
+        }
+        btnSound.play()
+    
+    }
 }
 
